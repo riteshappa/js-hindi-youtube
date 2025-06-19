777cc4031aa86d05bf396de7e65611356dae5de6
riteshappa <rm1705270@gmail.com>
2025-06-19 09:08:09 +0000

    new changes

---
+++ 04_control_flow/truthly.js
@@ -0,0 +1,10 @@
+//false value = false,0,-0,undefined,NaN,null,"",
+
+//true value = " ",'false',[],{}
+
+const emptyobj = {}
+if((Object.keys(emptyobj)).length==0)
+{
+    console.log("empty object");
+    
+}
---
+++ 05_iteration/01.js
@@ -0,0 +1,8 @@
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        console.log(`Detected 5`);
+        continue
+    }
+   console.log(`Value of i is ${index}`);
+    
+}
